5a6a903a429f94af32b7238acacbfd573a33d6e8
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2026-02-05 18:56:04 +0000

    final changes

---
+++ oop-mvc-id-20260205-b/Controllers/MembersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using oop_mvc_id_20260205_b.Data;
+using oop_mvc_id_20260205_b.Models;
+
+namespace oop_mvc_id_20260205_b.Controllers
+{
+    [Authorize(Roles = "Admin,Member")]
+    public class MembersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MembersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Members
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Movies.ToListAsync());
+        }
+
+        // GET: Members/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+       
+
+       
+
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movies.Any(e => e.Id == id);
+        }
+    }
+}
